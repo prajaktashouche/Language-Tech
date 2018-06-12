ed05667faff68788e5376731c861228fb7d93ced
aludi <ludivanleeuwen@gmail.com>
2018-06-12 15:06:41 +0200

    added selection and hard-coded out many/much

--- NLP.py
+++ NLP.py
@@ -19,13 +19,13 @@ class NLP:
         ret = []
         ##print(type(self.tokens))
         for w in range(0,len(self.tokens)):
-            print("word is " + str(self.tokens[w]) + " dep is " + str(self.tokens[w].dep_) + " tag is " + str(self.tokens[w].tag_))
+            print("word is " + str(self.tokens[w]) + " pos is " + str(self.tokens[w].pos_) + " dep is " + str(self.tokens[w].dep_) + " tag is " + str(self.tokens[w].tag_))
             print(w)
             if self.tokens[w].dep_ == dep and not self.tokens[w].text in list(self.specs.question_words.keys()) and not self.tokens[w].text.lower() in self.specs.ignored_words and str(self.tokens[w].tag_) in self.specs.tags_of_interest:
                 instance = []
                 instance.append(self.tokens[w].lemma_) #text to lemma
                 i = w-1
-                while self.tokens[i].dep_ == "compound" or self.tokens[i].dep_ == "amod": #added amod
+                while self.tokens[i].dep_ == "compound" or (self.tokens[i].dep_ == "amod" and (self.tokens[i].tag_ == 'JJ' or self.tokens[i].tag_ == 'JJS' or self.tokens[i].tag_ == 'JJR' or self.tokens[i].tag_ == 'NN') and not (self.tokens[i].text == 'many' or self.tokens[i].text == 'much')): #added amod
                     if self.tokens[i].dep_ == 'amod':
                         instance.append(self.tokens[i].text)  # add text, since lemma of amod is not a property
                     else:
